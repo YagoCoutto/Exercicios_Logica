f232e4fad37d520e5470d052f66eb5d5d4d5e559
Yago Coutto <62577739+YagoCoutto@users.noreply.github.com>
2022-06-24 23:46:22 -0300

    Desafios JS

--- Diferenca_BEE 1007/diferenca.js
+++ Diferenca_BEE 1007/diferenca.js
@@ -1,5 +1,3 @@
-
-
 let input = require("fs").readFileSync("Desafios_Beecrowd/Diferenca_BEE 1007/stdin","utf8");
 let valores = input.split("\n")
 
@@ -11,14 +9,3 @@ let D = parseInt(valores.shift());
 let DIFERENCA = (A*B) - (C*D);
 
 console.log(`DIFERENCA = ${DIFERENCA}`)
-
-
-/*
-Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
-
-Entrada
-O arquivo de entrada contém 4 valores inteiros.
-
-Saída
-Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
-*/
